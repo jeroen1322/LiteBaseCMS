e5330d0ca246a77de4828527afb5cf90521ffb0e
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-15 15:16:28 +0200

    Added Material Design

--- admin.php
+++ admin.php
@@ -12,12 +12,15 @@ require 'resources/functions.php';
     <head>
         <title>Admin Pannel - LiteBaseCMS</title>
         <link rel="stylesheet" type="text/css" href="resources/style/style.css">
+        <link type="text/css" rel="stylesheet" href="resources/style/materialize.min.css"  media="screen,projection"/>
     </head>
     <body>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="resources/js/materialize.min.js"></script>
         <div id="posts">
             <form action="admin.php" method="post">
                 <input type="text" name="title" placeholder="Title" autocomplete="off"><br><br>
-                <input type="text" name="text" placeholder="Add text" autocomplete="off">
+                <input type="text" name="text" placeholder="Add text" autocomplete="off"><br>
                 <input type="submit" name="submit" value="Submit">
             </form>
         </div>
