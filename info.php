cdf3f02a279fd9080d4e6f0e26def212394f1605
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-15 10:22:34 +0200

    Fixed 'no database selected' problem.

--- info.php
+++ info.php
@@ -8,4 +8,4 @@ $host = 'YOU HOST HERE';
 $user = 'YOUR USERNAME HERE';
 $passw = 'YOUR PASSWORD HERE';
 $db = 'YOUR DATABASE NAME HERE';
-$mysqli = new mysqli($host, $user, $passw);
+$mysqli = new mysqli($host, $user, $passw, $db);
