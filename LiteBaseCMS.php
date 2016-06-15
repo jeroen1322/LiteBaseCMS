e6671225ceda53b4bfcbd0fcaed864ef7349a3ea
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-15 10:50:55 +0200

    Added (__dir__) to require and connection check

--- LiteBaseCMS.php
+++ LiteBaseCMS.php
@@ -1,41 +1,30 @@
 <?php 
 
-require 'info.php';
-require 'functions.php';
+require (__dir__).'/info.php';
+require (__dir__).'/functions.php';
 
 //Check connection to the MYSQL server
 if($mysqli->connect_error){
     die("Connection failed: " . $mysqli->connect_error);
-}
-
-
-//If the database does not exist yet, create it by calling the createDB function
-$dbSelect = mysqli_select_db($mysqli, 'litebasecms');
-if(! $dbSelect){
-    createDB($mysqli);
-} else {
-    createTable($mysqli);
-    echo '
-        <form action="LiteBaseCMS.php" method="post">
-        <input type="text" name="title" placeholder="Title"><br><br>
-        <input type="text" name="text" placeholder="Put your text here"><br><br><br>
-        <input type="submit" name="submit" value="Submit">
-        </form>
-        ';
-}
-
-if(isset($_POST['submit'])){
+}else{
     
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-    
-    $insert = "INSERT INTO articles (title, text) VALUES ('$title', '$text');";
-    
-    if($mysqli->query($insert) === TRUE){
-        echo "The article was added succefully.";
-    } else {
-        echo "ERROR: " . $insert . "<br>" . $mysqli->error; 
+    if(isset($_POST['submit'])){
+
+        $title = $_POST['title'];
+        $text = $_POST['text'];
+
+        $insert = "INSERT INTO articles (title, text) VALUES ('$title', '$text');";
+
+        if($mysqli->query($insert) === TRUE){
+            echo "The article was added succefully.<br><br>";
+            echo "<a href='index.php'/><button>Go to homepage</button></a>";
+        } else {
+           echo "ERROR: " . $insert . "<br>" . $mysqli->error; 
+        }
     }
 }
+
+
+
 
 $mysqli->close();
